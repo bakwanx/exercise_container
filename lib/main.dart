977a8018ccc8a20472146d767bc4f473f945354a
Farhan <farhanfadhilah87@gmail.com>
2022-05-13 22:04:25 +0700

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -17,13 +17,24 @@ class MyApp extends StatelessWidget {
       home: SafeArea(
         child: Scaffold(
           body: Container(
-            padding: EdgeInsets.all(8),
-            color: Colors.green,
+            padding: EdgeInsets.all(25),
             child: Text(
               "Hai",
               style: TextStyle(
-                fontSize: 50
+                fontSize: 50,
+                color: Colors.white
               ),
+            ),
+            decoration: BoxDecoration(
+              color: Colors.red,
+              shape: BoxShape.circle,
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black,
+                  offset:Offset(3,6),
+                  blurRadius: 10
+                )
+              ]
             ),
           ),
         ),
